ca18996849702255ee1ff5581bbf0a78a78a6f51
idonotknowwhoiam <marcusymka@gmail.com>
2023-05-27 00:07:32 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,24 @@
+import keyboard
+from volume import reset_global_volume, set_volume, Event
+
+VOLUME_DOWN_KEY = -174
+VOLUME_UP_KEY = -175
+WIN_KEY = 91
+
+
+while True:  
+    keyboard.read_event()  
+    
+    event = None
+
+    if keyboard.is_pressed(VOLUME_DOWN_KEY) and keyboard.is_pressed(WIN_KEY):
+        event = Event.up
+        
+        reset_global_volume(event)
+        set_volume(event)
+    if keyboard.is_pressed(VOLUME_UP_KEY) and keyboard.is_pressed(WIN_KEY):
+        event = Event.down
+
+        reset_global_volume(event)
+        set_volume(event)
+
---
+++ volume.py
@@ -0,0 +1,48 @@
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume, IAudioEndpointVolume
+import ctypes
+import enum
+
+class Event(enum.Enum):
+    up = 2
+    down = 1
+
+VOLUME_STEP = 0.02
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, 0, None)
+volume_control = interface.QueryInterface(IAudioEndpointVolume)
+
+def set_volume(event):
+    foreground_window = ctypes.windll.user32.GetForegroundWindow()
+    active_window_pid = ctypes.wintypes.DWORD()
+    ctypes.windll.user32.GetWindowThreadProcessId(foreground_window, ctypes.byref(active_window_pid))
+
+    sessions = AudioUtilities.GetAllSessions()
+    for session in sessions:
+        volume_interface = session._ctl.QueryInterface(ISimpleAudioVolume)
+        process_id = session.Process.pid
+
+        if process_id == active_window_pid.value:
+            volume_level = volume_interface.GetMasterVolume()
+
+            if event == Event.down:
+                volume_level = clamp(volume_level + VOLUME_STEP, 0, 1)
+            elif event == Event.up:
+                volume_level = clamp(volume_level - VOLUME_STEP, 0, 1)
+
+            volume_interface.SetMasterVolume(volume_level, None)
+            break
+
+def reset_global_volume(event):
+    volume_level = volume_control.GetMasterVolumeLevelScalar()
+
+    if event == Event.down:
+        volume_level = clamp(volume_level - VOLUME_STEP, 0, 1)
+    if event == Event.up:
+        volume_level = clamp(volume_level + VOLUME_STEP, 0, 1)
+
+    volume_control.SetMasterVolumeLevelScalar(volume_level, None)
+
+def clamp(num, min, max):
+    return min if num < min else max if num > max else num
+
