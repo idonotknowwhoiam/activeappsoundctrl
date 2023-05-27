7d6949b32fd28d0bb4210580a82897755e30f464
idonotknowwhoiam <marcusymka@gmail.com>
2023-05-27 22:14:57 +0300

    Fix typo, create function

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from volume import reset_global_volume, set_active_volume
 
 VOLUME_UP_KEYCODE = -175
 VOLUME_DOWN_KEYCODE = -174
-DEFAUILT_VOLUME_STEP = 0.02
+DEFAULT_VOLUME_STEP = 0.02
 
 # your hotkeys
 volume_up_key = -175
 volume_down_key = -174
 modifier_key = "ctrl"
 
-volume_step = 0.02
+volume_step = DEFAULT_VOLUME_STEP
 
 while True:  
     keyboard.read_event()
     
     if keyboard.is_pressed(volume_down_key) and keyboard.is_pressed(modifier_key):
         if volume_down_key == VOLUME_DOWN_KEYCODE:
-            reset_global_volume(DEFAUILT_VOLUME_STEP)
+            reset_global_volume(DEFAULT_VOLUME_STEP)
 
         set_active_volume(-volume_step)
     if keyboard.is_pressed(volume_up_key) and keyboard.is_pressed(modifier_key):
         if volume_up_key == VOLUME_UP_KEYCODE:
-            reset_global_volume(-DEFAUILT_VOLUME_STEP) 
+            reset_global_volume(-DEFAULT_VOLUME_STEP) 
 
         set_active_volume(volume_step)
 
--- volume.py
+++ volume.py
@@ -6,9 +6,7 @@ interface = devices.Activate(IAudioEndpointVolume._iid_, 0, None)
 volume_control = interface.QueryInterface(IAudioEndpointVolume)
 
 def set_active_volume(volume):
-    foreground_window = ctypes.windll.user32.GetForegroundWindow()
-    active_window_pid = ctypes.wintypes.DWORD()
-    ctypes.windll.user32.GetWindowThreadProcessId(foreground_window, ctypes.byref(active_window_pid))
+    active_window_pid = get_active_window_pid()
 
     sessions = AudioUtilities.GetAllSessions()
     for session in sessions:
@@ -29,6 +27,12 @@ def reset_global_volume(volume):
     volume_level = clamp(current_level + volume, 0, 1)
 
     volume_control.SetMasterVolumeLevelScalar(volume_level, None)
+
+def get_active_window_pid():
+    foreground_window = ctypes.windll.user32.GetForegroundWindow()
+    active_window_pid = ctypes.wintypes.DWORD()
+    ctypes.windll.user32.GetWindowThreadProcessId(foreground_window, ctypes.byref(active_window_pid))
+    return active_window_pid
 
 def clamp(num, min, max):
     return min if num < min else max if num > max else num
